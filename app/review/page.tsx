92eca997bdb396e8f852042e501afbba4b5888fe
Aayush Thakur <athakur.1705096@gmail.com>
2025-08-13 22:09:17 -0400

    Update 4

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,11 +1,10 @@
 // app/review/page.tsx
-// @ts-expect-error - valid in Next.js app router
+
 export const dynamic = 'force-dynamic';
 export const revalidate = false;
-// @ts-expect-error - valid in Next.js app router
-import dynamic from 'next/dynamic';
-// @ts-expect-error - valid in Next.js app router
-const ReviewClient = dynamic(() => import('./review-client'), { ssr: false });
+
+
+const ReviewClient = (await import('./review-client')).default;
 
 export default function ReviewPage() {
   return <ReviewClient />;
